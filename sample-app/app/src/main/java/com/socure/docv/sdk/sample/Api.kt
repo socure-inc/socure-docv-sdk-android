faaa47f47259d8eddb18425c8ec5df55075a4db4
Vitaliy Akudovich <vitaliy.akudovich@socure.com>
2025-09-26 10:11:45 -0400

    Sample app version 5.2.1

--- sample-app/app/src/main/java/com/socure/docv/sdk/sample/Api.kt
+++ sample-app/app/src/main/java/com/socure/docv/sdk/sample/Api.kt
@@ -1,5 +1,6 @@
 package com.socure.docv.sdk.sample
 
+import com.socure.docv.capturesdk.BuildConfig
 import java.util.concurrent.TimeUnit
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
