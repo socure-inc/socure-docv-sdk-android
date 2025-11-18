25f1ee3c819ae14f22b16fb5ef1ac81230d00d4a
Vitaliy Akudovich <vitaliy.akudovich@socure.com>
2025-11-18 17:26:46 -0500

    Updated gradle to 8.14.3 and AGP to 8.12.3. Bumped buildSDK and targetSDK to 36

--- sample-app/app/src/main/java/com/socure/docv/sdk/sample/Api.kt
+++ sample-app/app/src/main/java/com/socure/docv/sdk/sample/Api.kt
@@ -1,6 +1,5 @@
 package com.socure.docv.sdk.sample
 
-import com.socure.docv.capturesdk.BuildConfig
 import java.util.concurrent.TimeUnit
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
